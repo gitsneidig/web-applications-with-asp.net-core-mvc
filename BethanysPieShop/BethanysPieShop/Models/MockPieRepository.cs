94dfdef091871b8925fb7a3947436613aed7959d
gitsneidig <scott@scottneidig.com>
2021-01-18 14:03:55 -0600

    Added category ID to mock pie repository

--- BethanysPieShop/BethanysPieShop/Models/MockPieRepository.cs
+++ BethanysPieShop/BethanysPieShop/Models/MockPieRepository.cs
@@ -13,8 +13,8 @@ namespace BethanysPieShop.Models
             {
             new Pie { PieId = 1, Name="Strawberry Pie", Price=15.99M, ShortDescription="Strawberry Pie Short", LongDescription="Strawberry Pie Long", Category = _categoryRepository.AllCategories.ToList()[0] },
             new Pie { PieId = 2, Name="Cheesecake", Price=18.99M, ShortDescription="Cheesecake Short", LongDescription="Cheesecake Long", Category = _categoryRepository.AllCategories.ToList()[1] },
-            new Pie { PieId = 3, Name="Rhubarb Pie", Price=15.99M, ShortDescription="Rhubarb Pie Short", LongDescription="Rhubarb Pie Long", Category = _categoryRepository.AllCategories.ToList()[2] },
-            new Pie { PieId = 4, Name="Pumpkin Pie", Price=12.99M, ShortDescription="Pumpkin Pie Short", LongDescription="Pumpkin Pie Long", Category = _categoryRepository.AllCategories.ToList()[3] }
+            new Pie { PieId = 3, Name="Rhubarb Pie", Price=15.99M, ShortDescription="Rhubarb Pie Short", LongDescription="Rhubarb Pie Long", Category = _categoryRepository.AllCategories.ToList()[0] },
+            new Pie { PieId = 4, Name="Pumpkin Pie", Price=12.99M, ShortDescription="Pumpkin Pie Short", LongDescription="Pumpkin Pie Long", Category = _categoryRepository.AllCategories.ToList()[2] }
             };
 
         public IEnumerable<Pie> PiesOfTheWeek { get; }
