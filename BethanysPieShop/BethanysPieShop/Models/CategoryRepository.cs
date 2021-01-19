7d9c5409e8283949f49917c5dc06014bb5a79fce
gitsneidig <scott@scottneidig.com>
2021-01-19 15:34:29 -0600

    Add DB model repositories for Pies and Categories

---
+++ BethanysPieShop/BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            appDbContext = _appDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => _appDbContext.Categories;
+    }
+}
