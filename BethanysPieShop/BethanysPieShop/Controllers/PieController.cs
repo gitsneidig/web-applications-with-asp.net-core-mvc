b17bb7ade430b1abe421cd91ea1f613a796a1f9d
gitsneidig <scott@scottneidig.com>
2021-01-19 20:28:01 -0600

    Create Details view and Controller->Action method, add namespaces to _ViewImports

--- BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -24,6 +24,14 @@ namespace BethanysPieShop.Controllers
             return View(piesListViewModel);
         }
 
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+            
+        }
 
     }
 }
