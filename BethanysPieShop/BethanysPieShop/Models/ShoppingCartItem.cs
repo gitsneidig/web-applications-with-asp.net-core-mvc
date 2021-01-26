258b1748bf96ee6e93f0bbd02a9b147fd08abbe3
gitsneidig <scott@scottneidig.com>
2021-01-25 23:18:17 -0600

    Updated spelling error

--- BethanysPieShop/BethanysPieShop/Models/ShoppingCartItem.cs
+++ BethanysPieShop/BethanysPieShop/Models/ShoppingCartItem.cs
@@ -10,6 +10,6 @@ namespace BethanysPieShop.Models
         public int ShoppingCartItemId { get; set; }
         public Pie Pie { get; set; }
         public int Amount { get; set; }
-        public string ShoppingCardId { get; set; }
+        public string ShoppingCartId { get; set; }
     }
 }
